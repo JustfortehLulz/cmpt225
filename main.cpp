fb4bb6fd428e70f40f790abf09c435c3210f309a
JustfortehLulz <32005739+JustfortehLulz@users.noreply.github.com>
2018-03-08 22:36:41 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,44 +5,44 @@ using namespace std;
 
 int main()
 {
-	Deque* weed = new Deque();
+	Deque* deque = new Deque();
 
-	// weed->push_left(6);
-	// weed->push_left(420);
-	// weed->push_left(3);
-	// weed->push_left(10);
+	// deque->push_left(6);
+	// deque->push_left(420);
+	// deque->push_left(3);
+	// deque->push_left(10);
 
-	//cout << weed->cursor_right() << endl;
-	//cout << weed->cursor_right() << endl;
-	//cout << weed->cursor_right() << endl;
-	//cout << weed->cursor_right() << endl;
+	//cout << deque->cursor_right() << endl;
+	//cout << deque->cursor_right() << endl;
+	//cout << deque->cursor_right() << endl;
+	//cout << deque->cursor_right() << endl;
 
-	weed->push_right(69);
-	weed->push_right(91);
-	weed->push_right(1);
-	weed->push_right(17);
+	deque->push_right(69);
+	deque->push_right(91);
+	deque->push_right(1);
+	deque->push_right(17);
 
-	cout << "This is the size: " << weed->size() << endl;
-	cout << "Is the queue empty? " << weed->empty() << endl;
-	cout << "This is popped: " << weed->pop_left() << endl;
-		cout << "THE BIGGEST WEED EVER " << weed->get_cursor() << endl;
-	cout << "This is popped: " << weed->pop_left() << endl;
-		cout << "THE BIGGEST WEED EVER " << weed->get_cursor() << endl;
-	cout << "This is popped: " << weed->pop_left() << endl;
+	cout << "This is the size: " << deque->size() << endl;
+	cout << "Is the queue empty? " << deque->empty() << endl;
+	cout << "This is popped: " << deque->pop_left() << endl;
+		cout << "THE BIGGEST deque EVER " << deque->get_cursor() << endl;
+	cout << "This is popped: " << deque->pop_left() << endl;
+		cout << "THE BIGGEST deque EVER " << deque->get_cursor() << endl;
+	cout << "This is popped: " << deque->pop_left() << endl;
 	
-	//cout << "BIG WEED " << weed->peek_left() << endl;
-	//cout << "EVEN BIGGER WEED " << weed->peek_left() << endl;
-	cout << "THE BIGGEST WEED EVER " << weed->get_cursor() << endl;
+	//cout << "BIG deque " << deque->peek_left() << endl;
+	//cout << "EVEN BIGGER deque " << deque->peek_left() << endl;
+	cout << "THE BIGGEST deque EVER " << deque->get_cursor() << endl;
 	// cout << "---NOW PRINTING LINKED LIST---" << endl;
 
-	//cout << "This is popped: " << weed->pop_right() << endl;
+	//cout << "This is popped: " << deque->pop_right() << endl;
 
-	//weed->display();
+	//deque->display();
 	//cout << endl;
-	//weed->verbose_display();
+	//deque->verbose_display();
 
-	// weed->~Deque();
+	// deque->~Deque();
 
-	//weed->display();
+	//deque->display();
 	return 0;
 }
